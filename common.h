90975fa250bebbbd0ee8d5a593ba2d0ae0b9b0ee
Lee Mitchell <lee@indigopepper.com>
2021-12-26 19:21:31 +0000

    Add copyright notices

--- common.h
+++ common.h
@@ -1,3 +1,24 @@
+/****************************************************************************
+ *
+ * Copyright 2021 Lee Mitchell <lee@indigopepper.com>
+ * This file is part of ADF435xCFG (ADF435x Configurator)
+ *
+ * ADF435xCFG (ADF435x Configurator) is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * ADF435xCFG (ADF435x Configurator) is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADF435xCFG (ADF435x Configurator).  If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+
 #ifndef COMMON_H
 #define COMMON_H
 
--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 /****************************************************************************
  *
  * Copyright 2021 Lee Mitchell <lee@indigopepper.com>
- * This file is part of OCC (Orlaco Camera Configurator)
+ * This file is part of ADF435xCFG (ADF435x Configurator)
  *
- * OCC (Orlaco Camera Configurator) is free software: you can redistribute it
+ * ADF435xCFG (ADF435x Configurator) is free software: you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
- * OCC (Orlaco Camera Configurator) is distributed in the hope that it will be
+ * ADF435xCFG (ADF435x Configurator) is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OCC (Orlaco Camera Configurator).  If not,
+ * along with ADF435xCFG (ADF435x Configurator).  If not,
  * see <http://www.gnu.org/licenses/>.
  *
  ****************************************************************************/
